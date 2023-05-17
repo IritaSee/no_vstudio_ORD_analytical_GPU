fef4ab9813718a478fe29d4f7145b4b64b9333c5
Iga Narendra <iga.narendra@gmail.com>
2023-05-17 13:45:22 +0900

    add exe to gitignore, add device sync

--- trial.cu
+++ trial.cu
@@ -7,13 +7,21 @@ __global__ void dkernel(){
 int main(){
     //syntax:
     //[global or host void name]<<<kernel invocation, kernel threads>>>();
-    dkernel<<<100,1000>>>(); //these are called kernels -> when called, are executed 32 times in parallel by 32 different CUDA threads, as opposed to only once like regular C functions.
+    dkernel<<<1,100>>>(); //these are called kernels -> when called, are executed 32 times in parallel by 32 different CUDA threads, as opposed to only once like regular C functions.
     /*
     A kernel is defined using the __global__ declaration specifier and the number of CUDA threads 
     that execute that kernel for a given kernel call is specified using a new <<<...>>> execution
     configuration syntax (see C Language Extensions). Each thread that executes the kernel is given a unique 
     thread ID that is accessible within the kernel through the built-in threadIdx variable.
     */
-    cudaThreadSynchronize();
+    cudaDeviceSynchronize();
+    /*
+    The CUDA code you have provided is syntactically correct. 
+    However, it will not print anything to the console. 
+    This is because the kernel is not actually running on the GPU. 
+    To run the kernel on the GPU, you need to call the cudaDeviceSynchronize() function. 
+    This function will ensure that the kernel has finished executing before the main() function returns.
+    */
+    //cudaThreadSynchronize(); -> deprecated
     return 0;
 }
