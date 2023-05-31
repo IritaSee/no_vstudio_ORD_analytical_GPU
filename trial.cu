12bf50e82331d8cd4ec1c728e3221da6098dcf3a
Iga-CML_Linux <iga.narendra@gmail.com>
2023-05-31 12:28:28 +0900

    fix merge mess

--- trial.cu
+++ trial.cu
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include <cuda.h>
 __global__ void dkernel(){
-    int tid = threadIdx.x;
-    printf("Hello Cuda %d\n", tid);
-
+    int t = threadIdx.x;
+    int T = blockIdx.x;
+    printf("tread index: %d  block index: %d\n",t,T);
 }
  // compile like usual c code (nvcc trial.cu -o trial) will create trial.exe
 int main(){
     //syntax:
-    //[global or host void name]<<<kernel invocation, kernel threads>>>();
-    dkernel<<<1,10>>>(); //these are called kernels -> when called, are executed 10 times in parallel by 10 different CUDA threads, 
-    // as opposed to only once like regular C functions.
+    //[global or host void name]<<<grid_dimension, block_dimension>>>();
+    int block_dim = 10; // how many threads are in one block
+    int grid_dim = 3; // hown many blocks are in one grid
+    dkernel<<<grid_dim,block_dim>>>(); //these are called kernels -> when called, are executed 32 times in parallel by 32 different CUDA threads, as opposed to only once like regular C functions.
     /*
-    A kernel is defined using the __global__ declaration specifier and the number of CUDA threads 
-    that execute that kernel for a given kernel call is specified using a new <<<...>>> execution
-    configuration syntax (see C Language Extensions). Each thread that executes the kernel is given a unique 
+     configuration syntax (see C Language Extensions). Each thread that executes the kernel is given a unique 
     thread ID that is accessible within the kernel through the built-in threadIdx variable.
     */
     cudaDeviceSynchronize();
@@ -28,3 +27,13 @@ int main(){
     //cudaThreadSynchronize(); -> deprecated
     return 0;
 }
+/*
+There are 4 variables each thread can access which contains information 
+about the organization of threads and current thread. They are:
+threadIdx : Id of the current thread.
+blockIdx : Id of the current block.
+blockDim : Size of each dimension of the current block.
+gridDim : Size of each dimension of the current grid.
+All of these are dim3 structure. we can use dot notation to access variable 
+x,y,z which contains the information of the corresponding dimension. Example: threadIdx.x
+*/
