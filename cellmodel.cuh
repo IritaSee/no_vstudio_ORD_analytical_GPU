273ba2b148a0e5c30a9c3f5b3daee25faf0ac48d
Iga-CML_Linux <iga.narendra@gmail.com>
2023-07-03 17:25:06 +0900

    prepare merge

--- cellmodel.cuh
+++ cellmodel.cuh
@@ -17,14 +17,14 @@ public:
   char GATES_HEADER[255];
   unsigned short *GATES_INDICES;
   virtual ~Cellmodel() {}
-  virtual __global__ void initConsts() = 0;
+  virtual void initConsts() = 0;
   virtual void initConsts(double type){}
   virtual void initConsts(bool is_dutta){}
   virtual void initConsts(double type, bool is_dutta){}
   virtual void initConsts(double type, double conc, double *hill){}
   virtual void initConsts(double type, double conc, double *hill, bool is_dutta){}
-  virtual __global__ void computeRates(double TIME, double *CONSTANTS, double *RATES, double *STATES, double *ALGEBRAIC) = 0;
-  virtual __global__ void solveAnalytical(double dt) {};
+  virtual void computeRates(double TIME, double *CONSTANTS, double *RATES, double *STATES, double *ALGEBRAIC) = 0;
+  virtual void solveAnalytical(double dt) {};
 };
 
 
